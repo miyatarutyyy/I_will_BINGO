fe1d867ed2edd66befad965bbd61d893692787cd
miyatarutyyy <study.miyata026@gmail.com>
2026-03-25 16:35:31 +0900

    Initial commit

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+// backend/src/app.ts
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ---------------------------------------------
+
+export default app;
